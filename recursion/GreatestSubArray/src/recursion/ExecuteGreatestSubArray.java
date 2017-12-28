b99fd1eb20917487c6968c10b4258302f3f7daf0
Vineeth Pradhan <vineeth.pradhan@cerner.com>
2017-12-28 20:41:04 +0530

    GreatestSubArray Finds the sum of two greatest numbers. Incomplete

--- recursion/GreatestSubArray/src/recursion/ExecuteGreatestSubArray.java
+++ recursion/GreatestSubArray/src/recursion/ExecuteGreatestSubArray.java
@@ -4,6 +4,9 @@ import recursion.GreatestSubArray;
 public class ExecuteGreatestSubArray {
 	public static void main(String[] args) {
 		GreatestSubArray gSubArray = new GreatestSubArray();
-		gSubArray.find(0, gSubArray.getA().length);
+		int[] greatestSubArray = gSubArray.find(0, gSubArray.getA().length - 1);
+		for(int i: greatestSubArray) {
+			System.out.println(i);
+		}
 	}
 }
--- recursion/GreatestSubArray/src/recursion/GreatestSubArray.java
+++ recursion/GreatestSubArray/src/recursion/GreatestSubArray.java
@@ -3,28 +3,57 @@ package recursion;
 public class GreatestSubArray {
 	int[] A = {13, -3, -25, 20, -3, -16, -23, 18,
 		20, -7, 12, -5, -22, 15, -4, 7};
-	int gSum, i, j;
+	int gSum = -1000000, i, j;
 
 	public int[] getA() {
 		return this.A;
 	}
 
-	public void find(int start, int end) {
+	public void setI(int i) {
+		this.i = i;
+	}
+
+	public void setJ(int j) {
+		this.j = j;
+	}
+
+	public int getI() {
+		return this.i;
+	}
+
+	public int getJ() {
+		return this.j;
+	}
+
+	public int getGSum() {
+		return this.gSum;
+	}
+
+	public int[] find(int start, int end) {
 		int mid = (start + end)/2;
-		if(end > start) {
+		if(start < end) {
 			find(start, mid);
-			sumUp(start, mid);
 			find(mid+1, end);
-			sumUp(mid+1, end);
+			sumUp(start, end);
 		}
+		int[] returnGreatestSum = {this.getI(), this.getJ(), this.getGSum()};
+		return returnGreatestSum;
 	}
 
 	public void sumUp(int i1, int j1) {
-		int thisSum = A[i1] + A[j1];
-		if(this.gSum < thisSum) {
-			this.gSum = thisSum;
-			this.i = i1;
-			this.j = j1;
+		if(i1 == j1) {
+			this.gSum = this.getA()[i1];
+			this.setI(i1);
+			this.setJ(j1);
+			return;
 		}
+		else{
+		    int thisSum = this.getA()[i1] + this.getA()[j1];
+		    if(this.gSum < thisSum) {
+			    this.gSum = thisSum;
+			    this.i = i1;
+			    this.j = j1;
+		    }
+	    }
 	}
 }
