13d195172d692d114fe06fe9a1d59bcebf968c44
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2017-12-30 22:02:56 +0530

    [GreatestSubArray] Bug fix

--- recursion/GreatestSubArray/src/recursion/GreatestSubArray.java
+++ recursion/GreatestSubArray/src/recursion/GreatestSubArray.java
@@ -1,7 +1,7 @@
 package recursion;
 
 public class GreatestSubArray {
-	int[] A = {13, -3, -25, 20, -3, -16, -23, 18,
+	int[] A = {13, 3, 25, 20, -3, -16, -23, 18,
 		20, -7, 12, -5, -22, 15, -4, 7};
 	int[] greatestMatrix = {-1000000, -1, -1};
 	
@@ -29,7 +29,7 @@ public class GreatestSubArray {
 		int sum = 0;
 		int i = j1;
 		int minLeft = -1, maxRight = -1;
-		while(i > i1) {
+		while(i >= i1) {
 			sum += A[i];
 			if(sum > leftSum) {
 				leftSum = sum;
@@ -40,7 +40,7 @@ public class GreatestSubArray {
 		int rightSum = -1111111;
 		i = j1+1;
 		sum = 0;
-		while(i < k1) {
+		while(i <= k1) {
 			sum += A[i];
 			if(sum > rightSum) {
 				rightSum = sum;
@@ -54,7 +54,7 @@ public class GreatestSubArray {
 	}
 
 	public int[] sumUp(int i1, int j1) {
-		int[] response = {-1111111, -1, -1};
+		int[] response = {};
 		if(i1 == j1) {
 			response = new int[]{A[i1], i1, j1};
 		}
@@ -64,7 +64,7 @@ public class GreatestSubArray {
 				sum += A[i];
 				if(sum > minSum) {
 					minSum = sum;
-					response = new int[]{minSum, i1, j1};
+					response = new int[]{minSum, i1, i};
 				}
 			}
 	    }
