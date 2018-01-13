4e10efd5009de3226aad0a9d31b77dcb4209cb2e
Vineeth Pradhan <vineeth.pradhan@cerner.com>
2018-01-13 19:49:43 +0530

    [heapsort]

--- src/algorithms/sorting/ExecuteHeapSort.java
+++ src/algorithms/sorting/ExecuteHeapSort.java
@@ -1,22 +1,23 @@
 package algorithms.sorting;
-import algorithms.data.ShortLeanRandomArray;
-import algorithms.sorting.core.heapsort.HeapSort;
-import algorithms.sorting.core.heapsort.MaxHeap;
-import algorithms.data.Node;
-import java.util.List;
+//import algorithms.data.ShortLeanRandomArray;
+//import algorithms.sorting.core.heapsort.HeapSort;
+//import algorithms.sorting.core.heapsort.MaxHeap;
+//import algorithms.data.Node;
+//import java.util.List;
 
 public class ExecuteHeapSort {
 
 	public static void main(String[] args) {
-		ShortLeanRandomArray A = new ShortLeanRandomArray();
-		int[] list = A.getList();
-		MaxHeap mh = new MaxHeap();
-		mh.heapify(list, 0);
-		List<Node> heapNodes = mh.get();
+//		ShortLeanRandomArray A = new ShortLeanRandomArray();
+		int[] list = {6, -1, 1, 7, 0, -6, -2, 3, -9, 4};
+//		int[] list = A.getList();
+//		MaxHeap mh = new MaxHeap();
+//		mh.heapify(list, 0);
+//		List<Node> heapNodes = mh.get();
 		
-		for(int i = 0; i < heapNodes.size(); i++) {
-			System.out.println(heapNodes.get(i).getValue());
-		}
+//		for(int i = 0; i < heapNodes.size(); i++) {
+//			System.out.println(heapNodes.get(i).getValue());
+//		}
 	}
 
 }
--- src/algorithms/sorting/core/heapsort/MaxHeap.java
+++ src/algorithms/sorting/core/heapsort/MaxHeap.java
@@ -1,27 +1,42 @@
 package algorithms.sorting.core.heapsort;
 
 import algorithms.data.Node;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+//import java.util.List;
 
+//6 -1 1 7 0 -6 -2 3 -9 4
+// 6(0), 1(1), -1(2),
 public class MaxHeap {
-	List<Node> nodes = new ArrayList<Node>();
+	LinkedList<Node> nodes = new LinkedList<Node>();
 	public void heapify(int[] list, int index) {
 		Node node = new Node(list[index]);
-		int largest;
-		if(index == 0) {
+		int largest = index;
+		if(nodes.size() == 0) {
 			nodes.add(node);
 		}
-		else {
-			if(nodes.get(index).getValue() > list[index]) {
-				largest = index;
-			}
-		}
-	}
-//			if(subHeap.get(parent).getValue() > subHeap.get(index).getValue()) {
-//				largest = parent;
+//		else {
+//			int lChild = index * 2; // 0
+//			int rChild = index * 2 + 1; // 1
+//			int parent = index / 2;	// 0
+//			try {
+//				if(nodes.get(index).getValue() < list[parent]) {
+//					largest = parent;
+//				}
+//				else if(nodes.get(index).getValue() < list[rChild]) {
+//					largest = rChild;
+//				}
+//				else {
+//					largest = lChild;
+//				}
 //			}
-	public List<Node> get(){
+//			catch(IndexOutOfBoundsException ex) {
+//				System.out.println("Out of bounds");
+//			}
+//		}
+		nodes.add(largest+1, node);
+//		heapify(list, largest); // 0
+	}
+	public LinkedList<Node> get(){
 		return this.nodes;
 	}
 }
